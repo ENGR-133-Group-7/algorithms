81cb84bac3ed71e806956ae5fb708809cfc44017
Lev Saunders <levsau00@gmail.com>
2020-05-13 20:19:27 -0400

    leds now blink when they detect a double error, fixed bug that caused cross_check() to not work

--- clock.py
+++ clock.py
@@ -4,7 +4,7 @@ import time
 import board
 i2c = busio .I2C( board .SCL , board .SDA)
 rtc = adafruit_pcf8523 . PCF8523 ( i2c )
-days = (" Sunday ", " Monday ", " Tuesday ", " Wednesday ", " Thursday ", " Friday ", "Saturday ")
+days = ("Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday")
 year = 2020 # Calendar Year
 month = 5 # Month (1= January , 12= December )
 date = 12 # Date
@@ -13,6 +13,6 @@ minute = 39 # Minute (0 -59)
 sec = 0 # Second (0 -59)
 wday = 0 # (0= Sunday , 6= Saturday )
 # This physically sets the clock
-t = time . struct_time ((year , month , date , hour , minute , sec , wday , -1 , -1))
-print (" Setting time to:",t)
-rtc. datetime = t
+ti = time . struct_time ((year , month , date , hour , minute , sec , wday , -1 , -1))
+print (" Setting time to:",ti)
+rtc. datetime = ti
--- code.py
+++ code.py
@@ -15,11 +15,11 @@ gc.enable()
 
 # initialize and mount the SD card
 def mount_SD():
-    spi = busio.SPI(board.SCK, board.MOSI, board.MISO)
-    cs = digitalio.DigitalInOut(board.D10)
-    sdcard = adafruit_sdcard.SDCard(spi, cs)
-    vfs = storage.VfsFat(sdcard)
-    storage.mount(vfs, "/sd")
+    spi= busio .SPI ( board .SCK , board .MOSI , board . MISO )
+    cs= digitalio . DigitalInOut ( board . D10)
+    sdcard = adafruit_sdcard . SDCard (spi , cs)
+    vfs= storage . VfsFat ( sdcard )
+    storage . mount (vfs , "/sd")
 
 #initialize humidity & temperature sensor
 def get_DHT(dht_pin):
@@ -31,6 +31,19 @@ def get_soil_sensor(i2c):
 #get photocell
 def get_photocell(photocell_pin):
     return analogio.AnalogIn(photocell_pin)
+
+def init_leds():
+
+    yellow = digitalio.DigitalInOut(board.D6)
+    blue = digitalio.DigitalInOut(board.D9)
+    green = digitalio.DigitalInOut(board.D11)
+    red = digitalio.DigitalInOut(board.D12)
+    yellow.direction = digitalio.Direction.OUTPUT
+    blue.direction = digitalio.Direction.OUTPUT
+    green.direction = digitalio.Direction.OUTPUT
+    red.direction = digitalio.Direction.OUTPUT
+
+    return yellow, blue, green, red
 
 #calibrates all sensors
 #def calibrate_sensors():
@@ -50,13 +63,13 @@ def log_values(t,dht,photocell,soil):
     photo_val = photocell.value
     volts = analog_voltage(photocell)
     moisture = soil.moisture_read()
-    soil_temp = soil.get_temp()
+    soil_temp = 1   # soil.get_temp()
 
     return (date,times,humidity,moisture,air_temp,soil_temp,photo_val,volts)
 
 # checks latest reading for values out of tolerance regions
 def check_log(log,POS):
-    bounds = [(1,2),(1,2),(25,28.5)] # tolerance region for humidity, moisture, and air temp
+    bounds = [(300,350),(300,350),(25,28.5)] # tolerance region for humidity, moisture, and air temp
     flags = [False for _ in range(3)]
     for pos in range(len(flags)):
         if log[pos+2]<bounds[pos][0] or log[pos+2]>bounds[pos][1]:
@@ -65,16 +78,34 @@ def check_log(log,POS):
     return tuple(flags)
 
 def cross_check(flags_log):
-    match = [(c and l) for c,l in zip(flags_log[-1],flags_log[-2])]
+    print('flags:',flags_log[-1],flags_log[-2])
+    try:
+        match = [(c and l) for c,l in zip(flags_log[-1],flags_log[-2])]
+    except Exception as e:
+        print('could not get two previous flag logs')
+    print('match data:',match)
+
     if match[0]:
-        print('humidity is off')
-        sys.exit()
+        print('\nhumidity is off\n')
+        for blink in range(2):
+            yellow.value = True
+            time.sleep(.05)
+            yellow.value = False
+            time.sleep(.05)
     if match[1]:
-        print('soil moisture is off')
-        sys.exit()
+        print('soil moisture is off\n')
+        for blink in range(2):
+            blue.value = True
+            time.sleep(.05)
+            blue.value = False
+            time.sleep(.05)
     if match[2]:
-        print('air temp is off')
-        sys.exit()
+        print('air temp is off\n')
+        for blink in range(2):
+            green.value = True
+            time.sleep(.05)
+            green.value = False
+            time.sleep(.05)
 
 #returns the past moisture reading where r is how many readings back is returned
 def get_past_moisture(logs,POS, r = 1):
@@ -83,6 +114,7 @@ def get_past_moisture(logs,POS, r = 1):
     except Exception as e:
         print('error in get_past_moisture():\n' + str(e))
 
+mount_SD()
 dht = get_DHT(board.D5)
 soil = get_soil_sensor(i2c)
 photocell = get_photocell(board.A1)
@@ -93,26 +125,30 @@ flags_log = [] #record of past flag logs
 max_len = 50 #maximum length for logs until older entries are overwritten
 count = 0
 
+yellow,blue,green,red = init_leds()
 
-#with open('/sd/DHT22_photocell_results.txt', "r+") as f:
-while count < 75:
-    count += 1
-    t = rtc.datetime
+with open("/sd/results.txt", "a") as f:
+    while count < 50:
+        count += 1
+        t = rtc.datetime
 
-    current_log = log_values(t,dht,photocell,soil)
-    if len(logs) < max_len:
-        logs.append(current_log)
-    else:
-        logs.pop(0)
-        logs.append(current_log)
+        current_log = log_values(t,dht,photocell,soil)
+        if len(logs) < max_len:
+            logs.append(current_log)
+        else:
+            logs.pop(0)
+            logs.append(current_log)
 
-    current_flags = check_log(current_log,POS)
-    if len(flags_log) < max_len:
-        flags_log.append(current_log)
-    else:
-        flags_log.pop(0)
-        flags_log.append(current_log)
+        current_flags = check_log(current_log,POS)
+        if len(flags_log) >= max_len:
+            flags_log.pop(0)
+        flags_log.append(current_flags)
+        if len(flags_log)>1:
+            cross_check(flags_log)
 
-    print(current_log)
-    print(len(logs))
-    time.sleep(0.15)
+        print(current_log)
+        print(len(logs))
+        time.sleep(0.15)
+        f.write(str(current_log) + '\n')
+
+[print(str(log[3])+'\n') for log in logs]
