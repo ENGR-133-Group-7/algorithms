c541653112a69d3e0684a4b14b99803ad5f882c4
Lev Saunders <levsau00@gmail.com>
2020-05-19 13:48:46 -0400

    added option for flowering, as well as other housekeeping changes

--- clock.py
+++ clock.py
@@ -7,12 +7,14 @@ rtc = adafruit_pcf8523 . PCF8523 ( i2c )
 days = ("Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday")
 year = 2020 # Calendar Year
 month = 5 # Month (1= January , 12= December )
-date = 12 # Date
-hour = 13 # Hour (0 -23)
-minute = 39 # Minute (0 -59)
+date = 19 # Date
+hour = 12 # Hour (0 -23)
+minute = 43 # Minute (0 -59)
 sec = 0 # Second (0 -59)
-wday = 0 # (0= Sunday , 6= Saturday )
+wday = 2 # (0= Sunday , 6= Saturday )
 # This physically sets the clock
 ti = time . struct_time ((year , month , date , hour , minute , sec , wday , -1 , -1))
 print (" Setting time to:",ti)
-rtc. datetime = ti
+
+rtc.datetime = ti
+print('init seconds: ',rtc.datetime[5])
--- code.py
+++ code.py
@@ -45,74 +45,90 @@ def init_leds():
 
     return yellow, blue, green, red
 
-#calibrates all sensors
-def calibrate_sensors():
-    print('\n\nsensors calibrated\n\n')
-
 #get analogue voltage
 def analog_voltage (adc):
     return (adc.value / 65535 * adc.reference_voltage)
 
+#calibrates all sensors
+def calibrate_sensors():
+    print('\n\nsensors calibrated\n\n')
+
+def correct_humidity():
+    print('\nmisting air with water to correct humidity\n')
+    for blink in range(2):
+            yellow.value = True
+            time.sleep(.05)
+            yellow.value = False
+            time.sleep(.05)
+def correct_moisture():
+    print('\nwatering plants to increase soil moisture\n')
+    for blink in range(2):
+            blue.value = True
+            time.sleep(.05)
+            blue.value = False
+            time.sleep(.05)
+def correct_temperature():
+    print('\nTemperature is off; adjust climate control\n')
+    for blink in range(2):
+            green.value = True
+            time.sleep(.05)
+            green.value = False
+            time.sleep(.05)
+
 #returns list of current values
 def log_values(t,dht,photocell,soil):
-    date = str (t.tm_mday) + '/' + str(t.tm_mon) + '/' + str(t.tm_year)
-    times = str(t.tm_hour) + ':' + str(t.tm_min) + ':' + str(t.tm_sec)
+    t_abs = rtc.datetime
+    date = str (t_abs.tm_mday) + '/' + str(t_abs.tm_mon) + '/' + str(t_abs.tm_year)
+    times = str(t_abs.tm_hour) + ':' + str(t_abs.tm_min) + ':' + str(t)
 
     air_temp = dht.temperature
     humidity = dht.humidity
     photo_val = photocell.value
     volts = analog_voltage(photocell)
-    moisture = soil.moisture_read()
+    moisture = moisture_to_percent(soil.moisture_read())
     soil_temp = 1   # soil.get_temp()
 
     return (date,times,humidity,moisture,air_temp,soil_temp,photo_val,volts)
 
 # checks latest reading for values out of tolerance regions
-def check_log(log,POS):
-    bounds = [(0,60),(300,400),(20,28.5)] # tolerance region for humidity, moisture, and air temp
+def check_log(log,POS,flowering):
+    bounds = [(65,85),(.2,.3),(21.1,26.8)] # tolerance region for humidity, moisture, and air temp
+    if flowering:
+        bounds[0] = (50,65)
     flags = [False for _ in range(3)]
     for pos in range(len(flags)):
-        if log[pos+2]<bounds[pos][0] or log[pos+2]>bounds[pos][1]:
-            flags[pos] = True
-            #print('out of tolerance region at pos =', (str(pos)+'!') )
+        if pos == 1:
+            if log[pos+2]<bounds[pos][0]:
+                flags[pos] = True
+        else:
+            if log[pos+2]<bounds[pos][0] or log[pos+2]>bounds[pos][1]:
+                flags[pos] = True
     return tuple(flags)
 
 def print_log(log, POS):
     print_str = ''
     for data,label in zip(log,POS):
-        print_str += label + ': ' + str(data) + '; '
+        if label is 'humidity':
+            print_str += label + ': ' + str(data) + '% ; '
+        elif label is 'moisture':
+            print_str += label + ': {:.1f}% ; '.format(data*100)
+        else:
+            print_str += label + ': ' + str(data) + ' ; '
     return print_str
 
 
 def cross_check(flags_log):
-    #print('flags:',flags_log[-1],flags_log[-2])
     try:
         match = [(c and l) for c,l in zip(flags_log[-1],flags_log[-2])]
     except Exception as e:
         print('could not get two previous flag logs')
-    #print('match data:',match)
 
-    if match[0]:
-        print('\nhumidity is off\n')
-        for blink in range(2):
-            yellow.value = True
-            time.sleep(.05)
-            yellow.value = False
-            time.sleep(.05)
+    if match[0] and not match[1]:
+        correct_humidity()
     if match[1]:
-        print('soil moisture is off\n')
-        for blink in range(2):
-            blue.value = True
-            time.sleep(.05)
-            blue.value = False
-            time.sleep(.05)
+        correct_moisture()
     if match[2]:
-        print('air temp is off\n')
-        for blink in range(2):
-            green.value = True
-            time.sleep(.05)
-            green.value = False
-            time.sleep(.05)
+        correct_temperature()
 
 #returns the past moisture reading where r is how many readings back is returned
 def get_past_moisture(logs,POS, r = 1):
@@ -120,6 +136,11 @@ def get_past_moisture(logs,POS, r = 1):
         return logs[-1*(1+r)][POS['moisture']]
     except Exception as e:
         print('error in get_past_moisture():\n' + str(e))
+#returns the percent moisture
+def moisture_to_percent(moisture):
+    percent = ((moisture - 200)/1800)
+    print('{:.1f}% moisture'.format(percent*100))
+    return percent
 
 mount_SD()
 dht = get_DHT(board.D5)
@@ -133,19 +154,17 @@ max_len = 50 #maximum length for logs until older entries are overwritten
 yellow,blue,green,red = init_leds()
 popped = False
 time_not_up = True
-current_time = 0
+flowering = False
 run_time = 30 #runtime in seconds
-
+ti = rtc.datetime[5]
 
 with open("/sd/results.txt", "w") as f:
     while time_not_up:
-        current_time += 1
-        t = rtc.datetime
-
-        if current_time % 5 == 0:                               #setting the interval for sensor calibration
+        t = rtc.datetime[5]-ti
+        if t % 5 == 0:                               #setting the interval for sensor calibration
             calibrate_sensors()
 
-        if current_time % 2 == 0:                               #setting the interval for logging data
+        if t % 2 == 0:                               #setting the interval for logging data
             current_log = log_values(t,dht,photocell,soil)
             if len(logs) < max_len:
                 logs.append(current_log)
@@ -153,7 +172,7 @@ with open("/sd/results.txt", "w") as f:
                 logs.pop(0)
                 logs.append(current_log)
 
-            current_flags = check_log(current_log,POS)
+            current_flags = check_log(current_log, POS, flowering)
 
             if len(flags_log) >= max_len:
                 if not popped:
@@ -166,9 +185,9 @@ with open("/sd/results.txt", "w") as f:
                 cross_check(flags_log)
 
             print_log(current_log,POS)
-            f.write('run time is ' + str(current_time) + ' seconds:\n     ' + print_log(current_log,POS) + '\n')
+            f.write('run time is ' + str(t) + ' seconds:\n     ' + print_log(current_log,POS) + '\n')
 
-        if current_time == run_time:
+        if t >= run_time:
             time_not_up = False
             print('\n\nruntime is complete\n\n')
         else:
